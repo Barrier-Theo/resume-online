375cb6257b7d30bda954a238f74cc39c92f537e3
Théo Barrier <theo.barrier@hotmail.fr>
2019-02-20 21:10:01 +0100

    First Commit

---
+++ js/resume.js
@@ -0,0 +1,36 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+	
+	$('#text-symfo').mouseover(function(){
+			$('#symfo').attr('src','img/symfony-hover.png');
+	});
+	
+	$('#text-symfo').mouseout(function(){
+			$('#symfo').attr('src','img/icons8-symfony-48.png');
+	});
+
+})(jQuery); // End of use strict
